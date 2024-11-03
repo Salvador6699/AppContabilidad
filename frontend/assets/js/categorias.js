08924998419c3d488a955582447aba96adbd7d0f
Ganbaru <ganbaru669@hotmail.com>
2024-11-03 08:44:23 +0100

    creamos y modificamos las vistas, home, cuentas y categorias

--- frontend/assets/js/categorias.js
+++ frontend/assets/js/categorias.js
@@ -1,22 +1,21 @@
-async function allCuentas() {
+async function allCategorias() {
     try {
-        let response = await fetch(urlJson + 'cuentas.json');
+        let response = await fetch(urlJson + 'categorias.json');
         let responseData = await response.json(); // Mover aquí para evitar duplicación
-        let cuentas = document.getElementById('cuentas');
+        let cuentas = document.getElementById('categorias');
         responseData.result.forEach((element) => {
-            let color = element.saldo < 0 ? '#e3b4aa ' : '#cceaca '; // Definir color aquí
+            
             // Insertar los datos dentro del contenedor
             cuentas.insertAdjacentHTML(
                 "beforeend",
                 `<div class="card">
-                    <h2>${element.cuenta}</h2>
-                    <h3 style="color:${color}"> ${element.saldo} €</h3> <!-- Corregir interpolación -->
+                    <h3>${element.nomCategoria}</h3><h5>(${element.tipoCategoria})</h5>
                 </div>`
             );
         });
     }
     catch (error) {
         console.error("No hay json:", error); // Manejo de errores
-        let json = await fetch(BASE_URL + 'json_cuentas');
+        let json = await fetch(BASE_URL + 'json_categorias');
     } 
 }
