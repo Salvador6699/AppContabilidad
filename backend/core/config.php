0814c38e2105e11d9d40815a71be17926dbc8c0d
Ganbaru <ganbaru669@hotmail.com>
2024-11-03 08:08:24 +0100

    añadimos variables globales para tener fecha actual, mes actual y año actual

--- backend/core/config.php
+++ backend/core/config.php
@@ -5,3 +5,7 @@ $request = $_SERVER['REQUEST_URI'];
 
 // Sanitizamos la URL para eliminar parámetros GET (si los hay)
 $request = strtok($request, '?');
+// Obtener la fecha actual
+$fechaActual = date('Y-m-d'); // Formato: Año-Mes-Día
+$mesActual = date('m'); // Mes actual (01 a 12)
+$anioActual = date('Y'); // Año actual
