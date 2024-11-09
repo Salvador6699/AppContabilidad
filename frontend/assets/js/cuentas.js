8aa626f4c8568d963a2a7985fda1d21ac5c2ca70
Ganbaru <ganbaru669@hotmail.com>
2024-11-09 12:03:13 +0100

    modificamos como ver las targetas de cuenta, estilo y una funcion para girarla y ver mas informacion

--- frontend/assets/js/cuentas.js
+++ frontend/assets/js/cuentas.js
@@ -4,13 +4,22 @@ async function allCuentas() {
         let responseData = await response.json(); // Mover aquí para evitar duplicación
         let cuentas = document.getElementById('cuentas');
         responseData.result.forEach((element) => {
-            let color = element.saldo < 0 ? '#e3b4aa ' : '#cceaca '; // Definir color aquí
+            let color = element.saldo < 0 ? 'var(--color-alerta)' : ''; // Definir color aquí
             // Insertar los datos dentro del contenedor
             cuentas.insertAdjacentHTML(
                 "beforeend",
-                `<div class="card">
-                    <h2>${element.cuenta}</h2>
-                    <h3 style="color:${color}"> ${element.saldo} €</h3> <!-- Corregir interpolación -->
+                `<div class="card" onclick="toggleCard(this);">
+                    <div class="card-inner">
+                        <div class="card-front" style="background: ${color};">
+                            <h3>${element.cuenta}</h3>
+                            <span>${element.saldo} €</span>
+                        </div>
+                        <div class="card-back">
+                            <p>${element.importe} €<br>
+                            ${element.categoria}:<br>
+                            ${element.concepto} (${element.fecha})</p>
+                        </div>
+                    </div>
                 </div>`
             );
         });
@@ -19,4 +28,17 @@ async function allCuentas() {
         console.error("No hay json:", error); // Manejo de errores
         let json = await fetch(BASE_URL + 'json_cuentas');
     } 
+}
+
+// Nueva función para manejar la animación de las tarjetas
+function toggleCard(card) {
+    // Cerrar cualquier tarjeta que esté abierta
+    const openCards = document.querySelectorAll('.card.animate');
+    openCards.forEach(openCard => {
+        if (openCard !== card) {
+            openCard.classList.remove('animate');
+        }
+    });
+    // Alternar la clase de la tarjeta actual
+    card.classList.toggle('animate');
 }
