72a4572b0accdeff0171377726b5d1c23745858a
Ganbaru <ganbaru669@hotmail.com>
2024-11-09 12:00:59 +0100

    -cambiamos la funcion getAll por ultimoMovimiento -añadimos la funcion fecha para formatear lo obtenido en fecha antes de incluirlo en el json

--- backend/controllers/CuentasController.php
+++ backend/controllers/CuentasController.php
@@ -1,6 +1,7 @@
 <?php
 require_once(__DIR__ . '/../core/ControllerViews.php');
 require_once(__DIR__ . '/../models/cuentasModel.php');
+require_once __DIR__ . '/../helper/formato.datos.php';
 class CuentasController extends ControllerViews
 {
     private $cuentasModel;
@@ -10,26 +11,33 @@ class CuentasController extends ControllerViews
     }
     public function getCuentas()
     {
-        $cuentas = $this->cuentasModel->getAll();
-          // Mapeo de los nombres de las columnas que deseas cambiar
-          $mapaColumnas = [
+        $cuentas = $this->cuentasModel->ultimoMovimiento();
+        // Formatear los datos antes de incluirlos en el JSON
+        foreach ($cuentas as &$cuenta) {
+            $cuenta['fechaMovimiento'] = fecha($cuenta['fechaMovimiento'], 'dia_mes');
+        }
+        // Mapeo de los nombres de las columnas que deseas cambiar
+        $mapaColumnas = [
             'idCuenta' => 'id',
             'nomCuenta' => 'cuenta',
             'saldoCuenta' => 'saldo',
             'usuarios_dniUsuario' => 'dni',
+            'fechaMovimiento' => 'fecha',
+            'importeMovimiento' => 'importe',
+            'subcategorias_nomSubcategoria' => 'concepto',
+            'subcategorias_categorias_nomCategoria'=>'categoria',
         ];
 
         // Renombrar las columnas usando la función genérica
         $cuentas = renombrarColumnas($cuentas, $mapaColumnas);
-        $res=new Result();
-        $res->message= count($cuentas) . ' registros de cuentas';
-        $res->success=true;
-        $res->result=$cuentas;
-        createJsonFile($res,'cuentas');
-        
+        $res = new Result();
+        $res->message = count($cuentas) . ' registros de cuentas';
+        $res->success = true;
+        $res->result = $cuentas;
+        createJsonFile($res, 'cuentas');
     }
     public function cuentasHome()
     {
-        $this->render(['cuentas'],[],'plantilla');
+        $this->render(['cuentas'], [], 'plantilla');
     }
 }
